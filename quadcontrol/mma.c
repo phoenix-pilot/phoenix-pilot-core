e389339a5af15f4fe198f01c61ff52fb3fe2a256
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2022-07-14 12:14:20 +0200

    quadcontrol/mma: all to arm and disarm motors using start & stop functions
    in details:
    This functionality is added to allow save exit from the control application.
    
    PP-24

--- quadcontrol/mma.c
+++ quadcontrol/mma.c
@@ -18,6 +18,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/threads.h>
 
 #include <syslog.h>
 #include <board_config.h>
@@ -28,6 +29,8 @@
 #define PWM_MIN_SCALER 100000
 
 struct {
+	unsigned int armed;
+	handle_t lock;
 	quad_coeffs_t coeffs;
 	FILE *files[NUMBER_MOTORS];
 } mma_common;
@@ -41,12 +44,19 @@ static const char *motorsPwm[] = {
 };
 
 
-void mma_control(float palt, float proll, float ppitch, float pyaw)
+int mma_control(float palt, float proll, float ppitch, float pyaw)
 {
 	int err;
 	unsigned int i;
 	uint32_t tmp;
 	float pwm[NUMBER_MOTORS];
+
+	mutexLock(mma_common.lock);
+	if (mma_common.armed == 0) {
+		fprintf(stderr, "mma: cannot set PWMs, module is disarmed\n");
+		mutexUnlock(mma_common.lock);
+		return -1;
+	}
 
 	pwm[0] = palt - proll + ppitch - pyaw;
 	pwm[1] = palt + proll + ppitch + pyaw;
@@ -66,16 +76,19 @@ void mma_control(float palt, float proll, float ppitch, float pyaw)
 
 		err = fprintf(mma_common.files[i], "%u\n", tmp);
 		if (err < 0) {
-			fprintf(stderr, "mma: cannot set PWM for motor: %u\n", i + 1);
+			fprintf(stderr, "mma: cannot set PWM for motor: %s\n", motorsPwm[i]);
 		}
 		fflush(mma_common.files[i]);
 	}
+	mutexUnlock(mma_common.lock);
 
 	DEBUG_LOG("PWM: %f, %f, %f, %f\n", pwm[0], pwm[1], pwm[2], pwm[3]);
+
+	return 0;
 }
 
 
-void mma_stop(void)
+static void _mma_motorsIdle(void)
 {
 	int err;
 	unsigned int i;
@@ -83,10 +96,35 @@ void mma_stop(void)
 	for (i = 0; i < NUMBER_MOTORS; ++i) {
 		err = fprintf(mma_common.files[i], "%d\n", PWM_MIN_SCALER);
 		if (err < 0) {
-			fprintf(stderr, "mma: cannot set PWM for motor: %u\n", i + 1);
+			fprintf(stderr, "mma: cannot set PWM for motor: %s\n", motorsPwm[i]);
 		}
 		fflush(mma_common.files[i]);
 	}
+}
+
+
+void mma_start(void)
+{
+	mutexLock(mma_common.lock);
+	/* Arm module */
+	mma_common.armed = 1;
+
+	_mma_motorsIdle();
+	mutexUnlock(mma_common.lock);
+
+	/* Wait for motor initialization */
+	sleep(2);
+}
+
+
+void mma_stop(void)
+{
+	mutexLock(mma_common.lock);
+	/* Disarm module */
+	mma_common.armed = 0;
+
+	_mma_motorsIdle();
+	mutexUnlock(mma_common.lock);
 }
 
 
@@ -94,11 +132,20 @@ void mma_done(void)
 {
 	unsigned int i;
 
+	mutexLock(mma_common.lock);
+
+	/* make sure that motors are stopped before closing dev files */
+	mma_common.armed = 0;
+	_mma_motorsIdle();
+
 	for (i = 0; i < NUMBER_MOTORS; ++i) {
 		if (mma_common.files[i] != NULL) {
 			fclose(mma_common.files[i]);
 		}
 	}
+	mutexUnlock(mma_common.lock);
+
+	resourceDestroy(mma_common.lock);
 }
 
 
@@ -106,6 +153,10 @@ int mma_init(const quad_coeffs_t *coeffs)
 {
 	unsigned int i;
 	int cnt, err = 0;
+
+	if ((err = mutexCreate(&mma_common.lock)) < 0) {
+		return err;
+	}
 
 	/* Initialize motors */
 	for (i = 0; i < NUMBER_MOTORS; ++i) {
@@ -119,22 +170,12 @@ int mma_init(const quad_coeffs_t *coeffs)
 			if (cnt > 10000) {
 				fprintf(stderr, "mma: timeout waiting on %s \n", motorsPwm[i]);
 				mma_done();
+				resourceDestroy(mma_common.lock);
 				return EXIT_FAILURE;
 			}
 			mma_common.files[i] = fopen(motorsPwm[i], "r+");
 		}
-
-		err = fprintf(mma_common.files[i], "%d\n", PWM_MIN_SCALER);
-		if (err < 0) {
-			fprintf(stderr, "mma: cannot set PWM for motor: %u\n", i + 1);
-			mma_done();
-			return EXIT_FAILURE;
-		}
-		fflush(mma_common.files[i]);
 	}
-
-	/* Wait for motor initialization */
-	sleep(2);
 
 	/* TODO: mma_common.coeffs for future usage */
 	memcpy(&mma_common.coeffs, coeffs, sizeof(quad_coeffs_t));
