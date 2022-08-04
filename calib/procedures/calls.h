b63b1ac2b92479d72c662eecffcbf772ec7a6b50
Mateusz Niewiadomski <mateusz.niewiadomski@phoenix-rtos.com>
2022-08-04 16:43:33 +0200

    calib: add mag calibration design sketch
    JIRA: PP-89

---
+++ calib/procedures/calls.h
@@ -0,0 +1,24 @@
+/*
+ * Phoenix-Pilot
+ *
+ * calib/procedures - calibration procedures
+ *
+ * Copyright 2022 Phoenix Systems
+ * Author: Mateusz Niewiadomski
+ *
+ * This file is part of Phoenix-Pilot software
+ *
+ * %LICENSE%
+ */
+
+#ifndef __PILOT_CALIB_CALLS_H__
+#define __PILOT_CALIB_CALLS_H__
+
+
+extern int cal_mMotCalib(void);
+
+extern int cal_mStaticCalib(void);
+
+extern int cal_aRotCalib(void);
+
+#endif
---
+++ calib/procedures/mcal_mot.c
@@ -0,0 +1,23 @@
+/*
+ * Phoenix-Pilot
+ *
+ * calib/procedures - magnetometer calibration with engines interference
+ *
+ * Copyright 2022 Phoenix Systems
+ * Author: Mateusz Niewiadomski
+ *
+ * This file is part of Phoenix-Pilot software
+ *
+ * %LICENSE%
+ */
+
+#include <stdio.h>
+
+#include "calls.h"
+
+
+int cal_mMotCalib(void)
+{
+	printf("Sorry, this calibration mode is not yet supported!\n");
+	return -1;
+}
