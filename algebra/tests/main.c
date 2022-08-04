45dfa1b01bb5f2ab0aee530a64c9219590dd93d6
Piotr Nieciecki <piotr.nieciecki@phoenix-rtos.com>
2022-08-04 13:53:42 +0200

    algebra: adding `matrix_zeroes` test
    JIRA: PP-71

---
+++ algebra/tests/main.c
@@ -0,0 +1,27 @@
+/*
+ * Phoenix-Pilot
+ *
+ * Unit tests for algebra operations
+ *
+ * Copyright 2022 Phoenix Systems
+ * Author: Piotr Nieciecki
+ *
+ * This file is part of Phoenix-Pilot software
+ *
+ * %LICENSE%
+ */
+
+#include <unity_fixture.h>
+
+
+void runner(void)
+{
+	RUN_TEST_GROUP(group_matrix_zeroes);
+}
+
+
+int main(int argc, char **argv)
+{
+	UnityMain(argc, (const char **)argv, runner);
+	return 0;
+}
