f70da66d0875581f801c1c361b45fd0bc1030079
Piotr Nieciecki <piotr.nieciecki@phoenix-rtos.com>
2022-08-23 17:44:26 +0200

    algebra: fixing `matrix_zeroes()` tests
    JIRA: PP-71

--- algebra/tests/matrix/various.c
+++ algebra/tests/matrix/various.c
@@ -24,7 +24,7 @@
 #define COLS 5
 
 static float buf[ROWS * COLS];
-static matrix_t A = { .data = buf, .cols = COLS, .rows = ROWS, .transposed = 0 };
+static matrix_t stMat = { .data = buf, .cols = COLS, .rows = ROWS, .transposed = 0 };
 
 
 /* ##############################################################################
@@ -48,13 +48,18 @@ TEST_TEAR_DOWN(group_matrix_zeroes)
 
 TEST(group_matrix_zeroes, matrix_zeroes_std)
 {
-	int i;
+	int row, col;
 
-	matrix_zeroes(&A);
+	matrix_zeroes(&stMat);
 
-	/* #TODO this should be done using function 'at' later */
-	for (i = 0; i < ROWS * COLS; i++) {
-		TEST_ASSERT_EQUAL_FLOAT(0.0, A.data[i]);
+	TEST_ASSERT_EQUAL_INT(ROWS, stMat.rows);
+	TEST_ASSERT_EQUAL_INT(COLS, stMat.cols);
+	TEST_ASSERT_FALSE(stMat.transposed);
+
+	for (row = 0; row < stMat.rows; row++) {
+		for (col = 0; col < stMat.cols; col++) {
+			TEST_ASSERT_EQUAL_FLOAT(0.0, *matrix_at(&stMat, row, col));
+		}
 	}
 }
 
