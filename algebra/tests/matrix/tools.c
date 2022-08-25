163cee10546b3b6e5e1925c4714905b3e585d43e
Piotr Nieciecki <piotr.nieciecki@phoenix-rtos.com>
2022-08-25 13:12:03 +0200

    algebra: moving functions used in matrix library tests to special file
    JIRA: PP-71

--- algebra/tests/matrix/tools.c
+++ algebra/tests/matrix/tools.c
@@ -43,3 +43,68 @@ void algebraTests_fillWithVal(matrix_t *M, float val)
 		}
 	}
 }
+
+
+int algebraTests_checkInvalidSeek(matrix_t *M)
+{
+	int rowsNum, colsNum, row, col;
+
+	algebraTests_getRowColNum(M, &rowsNum, &colsNum);
+
+	/* Both row and col outside matrix */
+	if (matrix_at(M, rowsNum, colsNum) != NULL) {
+		return CHECK_FAIL;
+	}
+	if (matrix_at(M, rowsNum + SMALL_SHIFT, colsNum + SMALL_SHIFT) != NULL) {
+		return CHECK_FAIL;
+	}
+	if (matrix_at(M, rowsNum + BIG_SHIFT, colsNum + BIG_SHIFT) != NULL) {
+		return CHECK_FAIL;
+	}
+
+	/* Only row outside matrix */
+	col = colsNum / 2; /* arbitrary position within columns */
+
+	if (matrix_at(M, rowsNum, col) != NULL) {
+		return CHECK_FAIL;
+	}
+	if (matrix_at(M, rowsNum + SMALL_SHIFT, col) != NULL) {
+		return CHECK_FAIL;
+	}
+	if (matrix_at(M, rowsNum + BIG_SHIFT, col) != NULL) {
+		return CHECK_FAIL;
+	}
+
+	/* Only col outside matrix */
+	row = rowsNum / 2; /* arbitrary position within rows */
+
+	if (matrix_at(M, row, colsNum) != NULL) {
+		return CHECK_FAIL;
+	}
+	if (matrix_at(M, row, colsNum + SMALL_SHIFT) != NULL) {
+		return CHECK_FAIL;
+	}
+	if (matrix_at(M, row, colsNum + BIG_SHIFT) != NULL) {
+		return CHECK_FAIL;
+	}
+
+	return CHECK_OK;
+}
+
+
+int algebraTests_checkMatrixZeroes(matrix_t *A)
+{
+	int rowsNum, colsNum, row, col;
+
+	algebraTests_getRowColNum(A, &rowsNum, &colsNum);
+
+	for (row = 0; row < rowsNum; row++) {
+		for (col = 0; col < colsNum; col++) {
+			if (*matrix_at(A, row, col) != 0.0) {
+				return CHECK_FAIL;
+			}
+		}
+	}
+
+	return CHECK_OK;
+}
