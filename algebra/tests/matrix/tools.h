ac6d15c86a6fac7d98391881c63e1d6212e72295
Piotr Nieciecki <piotr.nieciecki@phoenix-rtos.com>
2022-08-25 17:29:53 +0200

    algebra: refactoring `tools.c` library
    Refactoring due to new functions `matrix_rowsGet` and `matrix_colsGet`
    
    JIRA: PP-71

--- algebra/tests/matrix/tools.h
+++ algebra/tests/matrix/tools.h
@@ -22,10 +22,6 @@
 #define CHECK_FAIL -1
 
 
-/* Function rewrites rows and cols with number of rows and columns of passed matrix. */
-/* Function takes into considerations matrix transposition */
-extern void algebraTests_getRowColNum(matrix_t *M, int *rows, int *cols);
-
 /* Change every matrix element to val */
 /* Function does not change sizes and transposition of matrix */
 extern void algebraTests_fillWithVal(matrix_t *M, float val);
