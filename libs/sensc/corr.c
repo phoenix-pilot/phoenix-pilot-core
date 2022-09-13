eba5baab34017998b4be3c19130275f4ebb0a6ad
Mateusz Niewiadomski <mateusz.niewiadomski@phoenix-rtos.com>
2022-09-13 16:18:30 +0200

    sensc: add correction thread definitions
    JIRA: PP-89

--- libs/sensc/corr.c
+++ libs/sensc/corr.c
@@ -12,8 +12,12 @@
  */
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/threads.h>
 
 #include <calibcore.h>
 #include <hmap.h>
@@ -21,8 +25,20 @@
 #include "corr.h"
 
 
+typedef struct {
+	calib_t *cal;
+	time_t deadline;
+} corr_sched_t;
+
+
 struct {
 	hmap_t *corrs;
+
+	corr_sched_t *sched;
+	unsigned int schedSz;
+
+	char stack[4096];
+	volatile bool run;
 } corr_common;
 
 
@@ -35,9 +51,154 @@ void corr_register(calib_t *c)
 }
 
 
-hmap_t *corr_hashmapGet(void)
+void corr_done(void)
 {
-	return corr_common.corrs;
+	calib_t *c;
+	unsigned i;
+
+	/* stop recalculation thread */
+	// corr_common.run = false;
+	// threadJoin(0);
+
+	i = 0;
+	c = (calib_t *)hmap_next(corr_common.corrs, &i);
+	while (c != NULL) {
+		if (c->proc.corr.done != NULL) {
+			c->proc.corr.done();
+		}
+	}
+
+	free(corr_common.sched);
+}
+
+
+int corr_init(const char *path)
+{
+	unsigned int timeVar, init, i;
+	int retcode;
+	bool err = false;
+	calib_t *c;
+
+	if (calib_read(path, corr_common.corrs) < 0) {
+		return -EBADF;
+	}
+
+	/* count all time-variant corrections */
+	timeVar = i = 0;
+	c = (calib_t *)hmap_next(corr_common.corrs, &i);
+	while (c != NULL) {
+		if (c->proc.corr.delay != 0) {
+			timeVar++;
+		}
+		c = (calib_t *)hmap_next(corr_common.corrs, &i);
+	}
+
+	/* init all calibrations that need it */
+	init = i = 0;
+	c = (calib_t *)hmap_next(corr_common.corrs, &i);
+	while (c != NULL) {
+		if (c->proc.corr.init != NULL) {
+			retcode = c->proc.corr.init();
+			if (retcode != 0) {
+				err = true;
+				break;
+			}
+		}
+		init++;
+
+		c = (calib_t *)hmap_next(corr_common.corrs, &i);
+	}
+
+	/* if error occured, deinit all corrections up to the failed one */
+	if (err) {
+		i = 0;
+		while (init > 0) {
+			c = (calib_t *)hmap_next(corr_common.corrs, &i);
+			if (c->proc.calib.done != NULL) {
+				c->proc.calib.done();
+			}
+
+			init--;
+		}
+
+		return retcode;
+	}
+
+	/* initialize scheduler table for time variant corrections */
+	corr_common.sched = NULL;
+	corr_common.schedSz = timeVar;
+	if (timeVar > 0) {
+		corr_common.sched = calloc(timeVar, sizeof(corr_sched_t));
+		if (corr_common.sched == NULL) {
+			return -ENOMEM;
+		}
+	}
+
+	/* save all time-variant calibration procedures to newly created table */
+	i = 0;
+	while (timeVar > 0) {
+		c = (calib_t *)hmap_next(corr_common.corrs, &i);
+		if (c->proc.corr.delay != 0) {
+			corr_common.sched[timeVar - 1] = (corr_sched_t) { .cal = c, .deadline = c->proc.corr.delay };
+			timeVar--;
+		}
+	}
+
+	return EOK;
+}
+
+
+static void corr_thread(void *arg)
+{
+	calib_t *corr;
+	corr_sched_t *entry;
+	time_t now;
+	unsigned int i;
+
+	/* run all time-variant corrections for the first time */
+	for (i = 0; i < corr_common.schedSz; i++) {
+		entry = &corr_common.sched[i];
+		corr = entry->cal;
+
+		/* run recalculation */
+		corr->proc.corr.recalc();
+
+		/* reschedule */
+		gettime(&now, NULL);
+		entry->deadline = now + corr->proc.corr.delay;
+	}
+
+	/* Simple EDF scheduler */
+	if (corr_common.schedSz != 0) {
+		while (corr_common.run) {
+			/* find entry with earliest deadline */
+			entry = &corr_common.sched[0];
+			for (i = 1; i < corr_common.schedSz; i++) {
+				if (corr_common.sched[i].deadline < entry->deadline) {
+					entry = &corr_common.sched[i];
+				}
+			}
+			corr = entry->cal;
+
+			/* wait for the deadline */
+			gettime(&now, NULL);
+			if (entry->deadline > now) {
+				usleep(entry->deadline - now);
+			}
+
+			/* perform and reschedule current correction */
+			corr->proc.corr.recalc();
+			entry->deadline = now + corr->proc.corr.delay;
+		}
+	}
+
+	endthread();
+}
+
+
+int corr_run(void)
+{
+	return beginthread(corr_thread, 4, corr_common.stack, sizeof(corr_common.stack), NULL);
 }
 
 
--- libs/sensc/corr.h
+++ libs/sensc/corr.h
@@ -4,9 +4,16 @@
 #include <calibcore.h>
 
 /* registering new calibration/correction procedure */
-extern void corr_register(calib_t *c); 
+extern void corr_register(calib_t *c);
 
+/* starts the correction recalculation thread */
+int corr_run(void);
 
-extern hmap_t *corr_hashmapGet(void);
+/* initializes corrections and recalculation thread variables */
+int corr_init(const char *path);
+
+/* deinitializes correction module and stops the thread */
+void corr_done(void);
+
 
 #endif
