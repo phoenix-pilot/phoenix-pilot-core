819d1f7ba86594740086088d3987cdb61f7a9b37
Mateusz Niewiadomski <mateusz.niewiadomski@phoenix-rtos.com>
2022-09-14 12:38:08 +0200

    libcalib: introduce calibration library header and reading function stub
    JIRA: PP-89

---
+++ libs/calib/calib.c
@@ -0,0 +1,21 @@
+/*
+ * Phoenix-Pilot
+ *
+ * Calibration library
+ *
+ * Copyright 2022 Phoenix Systems
+ * Author: Mateusz Niewiadomski
+ *
+ * This file is part of Phoenix-Pilot software
+ *
+ * %LICENSE%
+ */
+
+#include "calib.h"
+
+
+/* read calibration file pointed by 'path' searching for calibration named `tag` and saving its content to 'cal' */
+int calib_readFile(const char *path, calibType_t type, calib_t *cal)
+{
+	return -1;
+}
