9bc619b3190f07700446ef34ec1dbff6f4027280
Mateusz Niewiadomski <mateusz.niewiadomski@phoenix-rtos.com>
2022-11-24 17:19:21 +0100

    ekf: simple, selective log module
    JIRA: PP-107

---
+++ ekf/log.c
@@ -0,0 +1,72 @@
+/*
+ * Phoenix-Pilot
+ *
+ * Ekf-specific log module
+ *
+ * Copyright 2022 Phoenix Systems
+ * Author: Mateusz Niewiadomski
+ *
+ * This file is part of Phoenix-Pilot software
+ *
+ * %LICENSE%
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include "log.h"
+
+
+struct {
+	uint32_t logFlags;
+	FILE *file;
+} ekflog_common;
+
+
+int ekflog_write(uint32_t flags, const char *format, ...)
+{
+	va_list args;
+	int ret = 0;
+
+	if (ekflog_common.file == NULL) {
+		return -1;
+	}
+
+	/* Log call with flags that are not enabled is not an error */
+	if ((flags & ekflog_common.logFlags) != 0) {
+		va_start(args, format);
+		ret = vfprintf(ekflog_common.file, format, args);
+		va_end(args);
+	}
+
+	return ret;
+}
+
+
+int ekflog_done(void)
+{
+	if (ekflog_common.file != NULL) {
+		return fclose(ekflog_common.file);
+	}
+
+	return 0;
+}
+
+
+int ekflog_init(const char *path, uint32_t flags)
+{
+	if (path == NULL) {
+		fprintf(stderr, "ekflog: wrong file path\n");
+		return -1;
+	}
+
+	ekflog_common.file = fopen(path, "w");
+	if (ekflog_common.file == NULL) {
+		fprintf(stderr, "ekflog: cant open %s to write\n", path);
+		return -1;
+	}
+
+	ekflog_common.logFlags = flags;
+
+	return 0;
+}
---
+++ ekf/log.h
@@ -0,0 +1,39 @@
+/*
+ * Phoenix-Pilot
+ *
+ * Ekf-specific log module
+ *
+ * Copyright 2022 Phoenix Systems
+ * Author: Mateusz Niewiadomski
+ *
+ * This file is part of Phoenix-Pilot software
+ *
+ * %LICENSE%
+ */
+
+
+#ifndef _EKF_LOG_
+#define _EKF_LOG_
+
+#include <stdio.h>
+#include <stdint.h>
+
+#define EKFLOG_SENSC   (1 << 0)
+#define EKFLOG_MEAS    (1 << 1)
+#define EKFLOG_EKF_IMU (1 << 2)
+#define EKFLOG_EKF_POS (1 << 3)
+
+
+/* Prints `flags` type log message passed as `format` */
+extern int ekflog_write(uint32_t flags, const char *format, ...);
+
+
+/* Deinitialize ekflog module */
+extern int ekflog_done(void);
+
+
+/* Initialize log module for `flags` log messages and `path` destination file. Returns 0 on success */
+extern int ekflog_init(const char *path, uint32_t flags);
+
+
+#endif
