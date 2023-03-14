08e6a8858f890ed6534599c4a253b4d4d5c10f38
Mateusz Niewiadomski <mateusz.niewiadomski@phoenix-rtos.com>
2023-03-14 09:34:26 +0100

    devekf: `prntVersor` refactor and `printPos` change
    - `prntVersor` mode is now properly calculating the attitude versors
     - `prntAcc` is changed to `printPos` as it is more usefull
    
    JIRA: PP-90

--- utils/devekf.c
+++ utils/devekf.c
@@ -23,20 +23,35 @@
 #include <quat.h>
 
 
-enum printMode { prntVersor, prntAtt, printAcc, silent };
+enum printMode { prntVersor, prntAtt, printPos, silent };
 
-
+/* Printing versors of local NED frame of reference in global ENU frame of reference */
 static void printUavVersors(ekf_state_t *uavState)
 {
-	vec_t start = { .x = uavState->enuX, .y = uavState->enuY, .z = uavState->enuZ };
-	vec_t x = { .x = 1, .y = 0, .z = 0 }, y = { .x = 0, .y = 1, .z = 0 }, z = { .x = 0, .y = 0, .z = 1 };
+	const quat_t ned2enu = { .a = 0, .i = -0.7071f, .j = -0.7071f, .k = 0 };
+
+	vec_t pos = { .x = uavState->enuX, .y = uavState->enuY, .z = uavState->enuZ };
+	vec_t versx = { .x = 1, .y = 0, .z = 0 }, versy = { .x = 0, .y = 1, .z = 0 }, versz = { .x = 0, .y = 0, .z = 1 };
 	quat_t q = { .a = uavState->q0, .i = uavState->q1, .j = uavState->q2, .k = uavState->q3 };
 
-	quat_vecRot(&x, &q);
-	quat_vecRot(&y, &q);
-	quat_vecRot(&z, &q);
+	/* Rotate versors from local to global frame of reference (all done in NED frame of reference) */
+	quat_vecRot(&versx, &q);
+	quat_vecRot(&versy, &q);
+	quat_vecRot(&versz, &q);
 
-	printf("%.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f\n", x.x, x.y, x.z, y.x, y.y, y.z, z.x, z.y, z.z, start.x, start.y, start.z);
+	/* Rotate versors from NED to ENU frame of reference */
+	quat_vecRot(&versx, &ned2enu);
+	quat_vecRot(&versy, &ned2enu);
+	quat_vecRot(&versz, &ned2enu);
+
+	/* Print NED versors in ENU frame of reference */
+	printf(
+		"%.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f ",
+		versx.x, versx.y, versx.z, versy.x, versy.y, versy.z, versz.x, versz.y, versz.z);
+
+	/* Position is already in ENU, just prinitng it */
+	printf("%.3f %.3f %.3f ", pos.x, pos.y, pos.z);
+	printf("\n");
 }
 
 
@@ -46,9 +61,13 @@ static inline void printUavAtt(ekf_state_t *uavState)
 }
 
 
-static inline void printUavAcc(ekf_state_t *uavState)
+static inline void printUavPos(ekf_state_t *uavState)
 {
-	printf("XYZ %f %f %f\n", uavState->accelX, uavState->accelY, uavState->accelZ);
+	printf("A %.0f %.1f %.1f ", uavState->yaw * 180 / 3.1415, uavState->pitch * 180 / 3.1415, uavState->roll * 180 / 3.1415);
+	printf("R %.1f %.2f %.2f ", uavState->yawDot, uavState->pitchDot, uavState->rollDot);
+	printf("P %.2f %.2f %.2f ", uavState->enuX, uavState->enuY, uavState->enuZ);
+	printf("V %.2f %.2f %.2f ", uavState->veloX, uavState->veloY, uavState->veloZ);
+	printf("\n");
 }
 
 
@@ -67,7 +86,7 @@ int main(int argc, char **argv)
 			mode = prntAtt;
 			break;
 		case '2':
-			mode = printAcc;
+			mode = printPos;
 			break;
 		case '3':
 			mode = silent;
@@ -91,8 +110,8 @@ int main(int argc, char **argv)
 		if (mode == prntAtt) {
 			printUavAtt(&uavState);
 		}
-		if (mode == printAcc) {
-			printUavAcc(&uavState);
+		if (mode == printPos) {
+			printUavPos(&uavState);
 		}
 		/* Mode = silent do not invoke any printing function */
 	}
