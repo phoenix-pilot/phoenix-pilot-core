da34571d70f96dcc689e8bc4b46ca38a4cb60306
Mateusz Niewiadomski <mateusz.niewiadomski@phoenix-rtos.com>
2023-06-12 21:34:36 +0200

    ekf: switch to qdiff.h functions
    Quaternion derivatives used local functions. They are now implemented in
    `qdiff.h` header.
    
    Leaving dv/dba and dv/dq unimplemented in final solution as they have to
    be tested more.
    
    JIRA: PP-88

--- ekf/kalman_implem.c
+++ ekf/kalman_implem.c
@@ -27,6 +27,7 @@
 
 #include <vec.h>
 #include <quat.h>
+#include <qdiff.h>
 #include <matrix.h>
 #include <parser.h>
 
@@ -227,120 +228,6 @@ static void kmn_stateEst(matrix_t *state, matrix_t *state_est, matrix_t *U, time
 	*matrix_at(state_est, RZ, 0) = kmn_vecAt(state, RZ) + dt * vState.z;
 }
 
-/* Calculates cross product matrix for vector v so that when left-hand-multiplied by a vector p produces cross product (v x p).
-* `out` is assumed to be 3x3, untransposed matrix.
-*/
-static inline void kmn_crossMat(const vec_t *v, matrix_t *out)
-{
-	out->data[0] = out->data[4] = out->data[8] = 0;
-	out->data[7] = v->x;
-	out->data[5] = -v->x;
-	out->data[2] = v->y;
-	out->data[6] = -v->y;
-	out->data[3] = v->z;
-	out->data[1] = -v->z;
-}
-
-/* Calculates half of derivative: d(q * p *cjg(q)) / dq with assumptions:
-* 1) `out` is 3x4 matrix
-* 2) `q` is rotation quaternion
-* 3) `p` is vector (or pure quaternion vectorial part)
-*/
-static void kmn_qpqDiffQ(const quat_t *q, const vec_t *p, matrix_t *out)
-{
-	float pCrossData[3 * 3] = { 0 };
-	float buffData[3 * 3] = { 0 };
-
-	/* Matrices for cross matrix of p and general matrix for the part of derivative over imaginary components of q */
-	matrix_t pCross = { .data = pCrossData, .rows = 3, .cols = 3, .transposed = 0 };
-	matrix_t buf = { .data = buffData, .rows = 3, .cols = 3, .transposed = 0 };
-
-	/* Vector for cross product (p x q) */
-	const vec_t qVec = { .x = q->i, .y = q->j, .z = q->k };
-	vec_t pxq;
-
-	vec_cross(p, &qVec, &pxq);
-
-	kmn_crossMat(p, &pCross);
-	kmn_crossMat(&pxq, &buf);
-
-	matrix_times(&pCross, q->a);
-	matrix_sub(&buf, &pCross, NULL);
-	buf.data[0] = buf.data[4] = buf.data[8] = vec_dot(p, &qVec);
-
-	matrix_writeSubmatrix(out, 0, 1, &buf);
-	*matrix_at(out, 0, 0) = q->a * p->x - pxq.x;
-	*matrix_at(out, 1, 0) = q->a * p->y - pxq.y;
-	*matrix_at(out, 2, 0) = q->a * p->z - pxq.z;
-}
-
-/* Calculates derivative: d(q * p * cjg(q)) / d(p) with assumptions:
-* 1) `out` is 3x3, untransposed matrix
-* 2) q is quaternion
-* Note: this derivative does not need value of `p`
-*/
-static void kmn_qpqDiffP(const quat_t *q, matrix_t *out)
-{
-	float qqtData[3 * 3] = { 0 };
-	matrix_t qqt = { .data = qqtData, .rows = 3, .cols = 3, .transposed = 0 };
-	const vec_t qVec = { .x = q->i, .y = q->j, .z = q->k };
-
-	kmn_crossMat(&qVec, out);
-	matrix_times(out, 2 * q->a);
-	/* Diagonal terms of `out` are now set to zeroes by `kmn_crossMat`. Proceed with diagonal terms */
-	out->data[0] = out->data[4] = out->data[8] = q->a * q->a - (q->i * q->i + q->j * q->j + q->k * q->k);
-
-	qqt.data[0] = q->i * q->i;
-	qqt.data[4] = q->j * q->j;
-	qqt.data[8] = q->k * q->k;
-
-	qqt.data[1] = qqt.data[3] = q->i * q->j;
-	qqt.data[2] = qqt.data[6] = q->i * q->k;
-	qqt.data[5] = qqt.data[7] = q->j * q->k;
-
-	matrix_times(&qqt, 2);
-	matrix_add(out, &qqt, NULL);
-}
-
-
-/* Calculates derivative: d(q * p) / d(q) with assumptions:
- * 1) `out` is 4x4 matrix
- * 2) 'q' and 'p' are quaternions
-*/
-static void kmn_qpDiffQ(quat_t *p, matrix_t *out)
-{
-	out->data[0] = out->data[5] = out->data[10] = out->data[15] = p->a;
-
-	out->data[4] = out->data[11] = p->i;
-	out->data[1] = out->data[14] = -p->i;
-
-	out->data[8] = out->data[13] = p->j;
-	out->data[2] = out->data[7] = -p->j;
-
-	out->data[12] = out->data[6] = p->k;
-	out->data[3] = out->data[9] = -p->k;
-}
-
-
-/* Calculates derivative: d(q * w) / d(q) with assumptions:
- * 1) `out` is 4x3 matrix
- * 2) 'q' is a quaternion
- * 3) 'w' is a quaternionized vector (only imaginary terms are significant)
-*/
-static void kmn_qwDiffW(const quat_t *q, matrix_t *out)
-{
-	out->data[3] = out->data[7] = out->data[11] = q->a;
-
-	out->data[10] = q->i;
-	out->data[0] = out->data[8] = -q->i;
-
-	out->data[5] = q->j;
-	out->data[1] = out->data[9] = -q->j;
-
-	out->data[6] = q->k;
-	out->data[2] = out->data[4] = -q->k;
-}
-
 
 /* prediction step jacobian calculation function */
 static void kmn_predJcb(matrix_t *F, matrix_t *state, matrix_t *U, time_t timeStep)
@@ -376,12 +263,12 @@ static void kmn_predJcb(matrix_t *F, matrix_t *state, matrix_t *U, time_t timeSt
 	quat_dif(&wMeas, &bwState, &p);
 	quat_times(&p, dt / 2);
 	p.a = 1;
-	kmn_qpDiffQ(&p, &dfqdq);
+	qvdiff_qpDiffQ(&p, &dfqdq);
 	/* d(f_q)/d(q) write into F */
 	matrix_writeSubmatrix(F, QA, QA, &dfqdq);
 
 	/* d(f_q)/d(bw) calculations */
-	kmn_qwDiffW(&qState, &dfqdbw);
+	qvdiff_qpDiffP(&qState, &dfqdbw);
 	matrix_times(&dfqdbw, -dt / 2);
 	/* d(f_q)/d(b_w) write into F */
 	matrix_writeSubmatrix(F, QA, BWX, &dfqdbw);
@@ -390,19 +277,19 @@ static void kmn_predJcb(matrix_t *F, matrix_t *state, matrix_t *U, time_t timeSt
 	*matrix_at(F, BWX, BWX) = *matrix_at(F, BWY, BWY) = *matrix_at(F, BWZ, BWZ) = 1;
 
 	/*
-	* SOMETHING IS WRONG in dfvdq or dfvdba!!! Disabling it as it is not position crucial derivative.
-	* It must be taken care of!!
+	* DISABLED USE OF POSITION DERIVATIVES
+	* Not yet tested in flight!
 	*/
 
 	/* d(f_v)/d(q) calculations */
 	vec_dif(&aMeas, &baState, &aTrue);
-	kmn_qpqDiffQ(&qState, &aTrue, &dfvdq);
+	qvdiff_qvqDiffQ(&qState, &aTrue, &dfvdq);
 	matrix_times(&dfvdq, 2 * dt);
 	/* d(f_v)/d(q) write into F */
 	/* matrix_writeSubmatrix(F, VX, QA, &dfvdq); */
 
 	/* d(f_v)/d(ba) calculations */
-	kmn_qpqDiffP(&qState, &dfvdba);
+	qvdiff_qvqDiffV(&qState, &dfvdba);
 	matrix_times(&dfvdba, -dt);
 	/* d(f_v)/d(ba) write into F */
 	/* matrix_writeSubmatrix(F, VX, BAX, &dfvdba); */
