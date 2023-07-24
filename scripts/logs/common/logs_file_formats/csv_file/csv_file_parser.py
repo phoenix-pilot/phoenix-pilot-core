a0f5004be3eef3bf0ef038b6e998d708b9047323
Piotr Nieciecki <piotr.nieciecki@phoenix-rtos.com>
2023-07-24 16:54:54 +0200

    logs_converter: add `csv` file format support
    JIRA: PP-161

---
+++ scripts/logs/common/logs_file_formats/csv_file/csv_file_parser.py
@@ -0,0 +1,67 @@
+import csv
+import common.models.log_reading as logs_types
+import common.models.utils as utils
+
+
+class CsvLogParser:
+    def parse(self, file_path: str):
+        result = []
+
+        with open(file_path, "r") as file:
+            csv_reader = csv.reader(file, delimiter=',')
+            for row in csv_reader:
+                if len(row) < 2:
+                    continue
+
+                if row[1] == "Imu":
+                    result.append(self.__parse_imu_log(row))
+                elif row[1] == "Gps":
+                    result.append(self.__parse_gps_log(row))
+                elif row[1] == "Time":
+                    result.append(self.__parse_time_log(row))
+                elif row[1] == "Baro":
+                    result.append(self.__parse_baro_log(row))
+                else:
+                    print(f"Unknown entry in log file {row}")
+
+        return result
+
+
+    def __parse_imu_log(self, row: list[str]) -> logs_types.ImuLog:
+        id = int(row[0])
+        timestamp = int(row[2])
+        accel = utils.Vector3(int(row[3]), int(row[4]), int(row[5]))
+        gyro = utils.Vector3(int(row[6]), int(row[7]), int(row[8]))
+        gyroDAngle = utils.Vector3(int(row[9]), int(row[10]), int(row[11]))
+        mag = utils.Vector3(int(row[12]), int(row[13]), int(row[14]))
+
+        return logs_types.ImuLog(id, timestamp, accel, gyro, gyroDAngle, mag)
+
+
+    def __parse_gps_log(self, row: list[str]) -> logs_types.GpsLog:
+        id = int(row[0])
+        timestamp = int(row[2])
+        position = utils.GlobalPosition(int(row[3]), int(row[4]), int(row[5]))
+        horizontalAcc = int(row[6])
+        velocityAcc = int(row[7])
+        fix = int(row[8])
+        satelliteNumber = int(row[9])
+        velocity = utils.NEDCoordinates(int(row[10]), int(row[11]), int(row[12]))
+
+        return logs_types.GpsLog(id, timestamp, position, horizontalAcc, velocityAcc, satelliteNumber, fix, velocity)
+
+
+    def __parse_time_log(self, row: list[str]) -> logs_types.TimeLog:
+        id = int(row[0])
+        timestamp = int(row[2])
+
+        return logs_types.TimeLog(id, timestamp)
+
+
+    def __parse_baro_log(self, row: list[str]) -> logs_types.BaroLog:
+        id = int(row[0])
+        timestamp = int(row[2])
+        pressure = int(row[3])
+        temperature = int(row[4])
+
+        return logs_types.BaroLog(id, timestamp, pressure, temperature)
