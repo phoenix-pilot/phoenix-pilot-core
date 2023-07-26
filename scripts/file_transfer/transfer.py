32ca49ac49aa05336a747f5e122d2039fce5ef4e
Piotr Nieciecki <piotr.nieciecki@phoenix-rtos.com>
2023-07-26 18:24:53 +0200

    log_transfer: allow using different serial ports for console and data transfer
    JIRA: PP-160

--- scripts/file_transfer/transfer.py
+++ scripts/file_transfer/transfer.py
@@ -1,6 +1,6 @@
-import pexpect
 import argparse
 import subprocess
+import serial
 import os
 import re
 
@@ -14,48 +14,104 @@ TMP_FILE = "/tmp/file_transfer.bz2.b64"
 def get_args():
     parser = argparse.ArgumentParser(description="Transfers file from device to host")
 
-    parser.add_argument("source_path", type=str,
-                        help="File from the device, which should be transferred")
-    parser.add_argument("dest_path", type=str,
-                        help="Path to destination file on host")
-    parser.add_argument("-b", "--baud", type=int, required=True, dest="baudrate",
-                        help="Baudrate")
-    parser.add_argument("-t", "--tty", type=str, required=True, dest="tty",
-                        help="Path to TTY port device")
+    parser.add_argument(
+        "source_path",
+        type=str,
+        help="File from the device, which should be transferred"
+    )
+    parser.add_argument(
+        "dest_path",
+        type=str,
+        help="Path to destination file on host"
+    )
+    parser.add_argument(
+        "-p",
+        "--portrx",
+        required=False,
+        default="/dev/ttyUSB0",
+        help="host receiving port",
+    )
+    parser.add_argument(
+        "-P",
+        "--portc",
+        required=False,
+        default="/dev/ttyUSB1",
+        help="host commandline port",
+    )
+    parser.add_argument(
+        "-b",
+        "--baudx",
+        required=False,
+        default=57600,
+        type=int,
+        help="transfer baudrate",
+    )
+    parser.add_argument(
+        "-B",
+        "--baudc",
+        required=False,
+        default=57600,
+        type=int,
+        help="commandline baudrate",
+    )
+    parser.add_argument(
+        "-t",
+        "--porttx",
+        required=False,
+        default="/dev/uart1",
+        help="target transfer port",
+    )
 
     return parser.parse_args()
+
+
+def start_transition(console_port: str, baudrate: int, source_path: str, device_transfer_port: str):
+    with serial.Serial(console_port, baudrate=baudrate, timeout=1) as device_console:
+        command = f"sleep 1 ; bzip2 -k9fc {source_path} | base64 > {device_transfer_port}"
+
+        device_console.write(bytes(command + "\n", "ascii"))
+        device_console.readline()
+
+
+def receive_data(receive_port: str, baudrate: int):
+    with serial.Serial(receive_port, baudrate=baudrate, timeout=5) as receive:
+        with open(TMP_FILE, "w", newline="") as file:
+            while True:
+                line = receive.readline().decode("ascii")
+
+                if re.match(PROMPT, line) is not None or len(line) == 0:
+                    break
+
+                file.write(line.removesuffix("\r\n"))
+
+
+def decode_data(dest_path: str):
+    with open(dest_path, "w") as dest_file:
+        ps = subprocess.Popen(["base64", "-d", TMP_FILE], stdout=subprocess.PIPE)
+        subprocess.run(["bunzip2", "-fcq9"], stdin=ps.stdout, stdout=dest_file)
+
+    os.remove(TMP_FILE)
 
 
 def main() -> None:
     args = get_args()
 
-    device = pexpect.spawn(f"picocom -b {args.baudrate} --imap lfcrlf {args.tty}", encoding="utf-8")
-    device.expect("Terminal ready")
+    try:
+        start_transition(args.portc, args.baudc, args.source_path, args.porttx)
+    except Exception:
+        print(f"Error while writing to {args.portc}")
 
-    device.sendline()
-    device.expect(PROMPT, timeout=3)
+    try:
+        receive_data(args.portrx, args.baudx)
+    except serial.SerialException as e:
+        print(f"Error while writing to {args.portrx}: {e}")
+    except IOError as e:
+        print(f"Error while writing to file: {e}")
 
-    if device.match.group(1) != "~":
-        device.sendline("cd ~")
-        device.expect(PROMPT)
-
-    with open(TMP_FILE, "w", newline="") as file:
-        command = f"bzip2 -k9fc {args.source_path} | base64"
-        device.sendline(command)
-
-        espaced = re.escape(command)
-        device.expect(f"{espaced}{EOL}")
-
-        while (device.expect([f"{EOL}", PROMPT]) == 0):
-            file.write(f"{device.before}")
-
-    with open(args.dest_path, "w") as dest_file:
-        ps = subprocess.Popen(["base64", "-di", TMP_FILE], stdout=subprocess.PIPE)
-        subprocess.run(["bunzip2", "-fcq9"], stdin=ps.stdout, stdout=dest_file)
-
-    os.remove(TMP_FILE)
-
-    device.terminate()
+    try:
+        decode_data(args.dest_path)
+    except Exception as e:
+        print(f"Error while decoding data: {e}")
 
 
 if __name__ == "__main__":
