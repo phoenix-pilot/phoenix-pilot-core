c3d28118e524fa22e1c4eb457b55dcfc166c617a
Piotr Nieciecki <piotr.nieciecki@phoenix-rtos.com>
2023-10-27 15:56:50 +0200

    ekf log script: move geodetic distance calculations to seperate file
    JIRA: PP-189

---
+++ scripts/ekf_logs/logs_evaluations/evaluations/gps_ekf_comparison/geo_distance.py
@@ -0,0 +1,38 @@
+from geopy import distance
+
+
+class GeodeticDistance:
+    def __init__(self, origin_latitude=0, origin_longitude=0) -> None:
+        self.originLat = origin_latitude
+        self.originLon = origin_longitude
+
+    @staticmethod
+    def distance(latitude1, longitude1, latitude2, longitude2):
+        return distance.geodesic(
+            (latitude1, longitude1),
+            (latitude2, longitude2),
+            ellipsoid='WGS-84'
+        ).m
+
+    def dist_from_origin(self, latitude, longitude):
+        return self.distance(self.originLat, self.originLon, latitude, longitude)
+
+    def latitude_diff(self, latitude):
+        """Returns latitude difference in meters. If passed latitude is smaller than origin, the result is negative"""
+
+        res = self.dist_from_origin(latitude, self.originLon)
+
+        if self.originLat > latitude:
+            res = -res
+
+        return res
+
+    def longitude_diff(self, longitude):
+        """Returns latitude difference in meters. If passed latitude is smaller than origin, the result is negative"""
+
+        res = self.dist_from_origin(self.originLat, longitude)
+
+        if self.originLon > longitude:
+            res = -res
+
+        return res
