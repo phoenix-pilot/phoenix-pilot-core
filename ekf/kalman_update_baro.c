20729d311908e02ddba52d3b0e6a37a371030398
Mateusz Niewiadomski <mateusz.niewiadomski@phoenix-rtos.com>
2023-11-14 16:51:39 +0100

    ekf: reduce baro update
    JIRA: PP-200

--- ekf/kalman_update_baro.c
+++ ekf/kalman_update_baro.c
@@ -34,10 +34,6 @@
 /* Returns pointer to passed Z matrix filled with newest measurements vector */
 static matrix_t *getMeasurement(matrix_t *Z, matrix_t *state, matrix_t *R, time_t timeStep)
 {
-	static time_t lastTstamp = 0;
-	static float lastAlt = 0;
-	static float lpfAltChange = 0;
-
 	float pressure, temp, alt;
 	uint64_t currTstamp;
 
@@ -46,19 +42,11 @@ static matrix_t *getMeasurement(matrix_t *Z, matrix_t *state, matrix_t *R, time_
 		return NULL;
 	}
 
-	if (currTstamp <= lastTstamp) {
-		return NULL;
-	}
-
 	/* Make measurements negative to account for NED frame convention */
 	alt = -8453.669 * log(meas_calibPressGet() / pressure);
-	lpfAltChange = (alt - lastAlt);
-	fltr_vBaroLpf(&lpfAltChange);
+	fltr_vBaroLpf(&alt);
 
-	Z->data[MDZ] = lpfAltChange;
 	Z->data[MRZ] = alt;
-
-	lastAlt = alt;
 
 	return Z;
 }
@@ -66,13 +54,6 @@ static matrix_t *getMeasurement(matrix_t *Z, matrix_t *state, matrix_t *R, time_
 
 static matrix_t *getMeasurementPrediction(matrix_t *state_est, matrix_t *hx, time_t timestep)
 {
-	const float dt = (float)timestep / 1000000.f;
-
-	vec_t deltaR = { .x = kmn_vecAt(state_est, VX), .y = kmn_vecAt(state_est, VY), .z = kmn_vecAt(state_est, VZ) };
-
-	vec_times(&deltaR, dt);
-
-	hx->data[MDZ] = deltaR.z;
 	hx->data[MRZ] = kmn_vecAt(state_est, RZ);
 
 	return hx;
@@ -81,12 +62,8 @@ static matrix_t *getMeasurementPrediction(matrix_t *state_est, matrix_t *hx, tim
 
 static void getMeasurementPredictionJacobian(matrix_t *H, matrix_t *state, time_t timeStep)
 {
-	const float dt = (float)timeStep / 1000000.f;
-
 	matrix_zeroes(H);
 
-	/* d(dz)/d(v) calculations */
-	*matrix_at(H, MDZ, VZ) = dt;
 	*matrix_at(H, MRZ, RZ) = 1;
 }
 
@@ -95,12 +72,6 @@ static void getMeasurementPredictionJacobian(matrix_t *H, matrix_t *state, time_
 static void baroUpdateInitializations(matrix_t *H, matrix_t *R, const kalman_init_t *inits)
 {
 	matrix_zeroes(R);
-
-	/*
-	Its easy to prove that if we measure change of height using gaussian noised height measurement
-	then variance (standard deviation squared) of such height change is twice the squared height standard deviation.
-	*/
-	*matrix_at(R, MDZ, MDZ) = inits->R_hstdev * inits->R_hstdev * 2;
 
 	*matrix_at(R, MRZ, MRZ) = inits->R_hstdev * inits->R_hstdev;
 }
